0ed9bac5b6665570a33c7b0350d9c7192668b427
FateScript <422643055@qq.com>
2018-10-14 00:48:35 +0800

    finish LDA

--- LDA.py
+++ LDA.py
@@ -2,7 +2,9 @@
 # -*- coding:utf-8 -*-
 
 import numpy as np
+import math
 import matplotlib.pyplot as plt
+#from .svd import svd
 
 def SVD(M, use_np=True):
     if use_np:
@@ -16,24 +18,21 @@ class LDA:
         pass
 
     def getSw(self, x0, x1):
-        x0_cov = np.cov(x0)
-        x1_cov = np.cov(x1)
+        x0_cov = np.cov(np.transpose(x0))
+        x1_cov = np.cov(np.transpose(x1))
         return x0_cov + x1_cov
 
     def getSb(self):
         diff_vec = np.matrix(self.x0_mean - self.x1_mean)
-        #type of Sb is np.matrix
         return diff_vec.T * diff_vec
 
     def inverse(self, S):
-        #S = np.matrix(S)
+        S = np.matrix(S)
         u,s,v = SVD(S)
-        print("s:",s)
-        return (np.multiply(1/s,v.T)) * u.T
-
+        return np.array( (np.multiply(1/s,v.T)) * u.T )
 
     def fit(self, x0, x1):
-        #LDA minimize generalized Rayleigh quotient
+        #LDA maximize generalized Rayleigh quotient
         #generalized Rayleigh quotient: w.T * Sb * w / w.T * Sw *w
         #Sw = sigma0 + sigma1 while sigma0 is the covariance matrix of class0,sigma1 similarly
         #Sw is also named within-class scatter matrix
@@ -42,9 +41,9 @@ class LDA:
         self.x0_mean = np.mean(x0, axis=0)
         self.x1_mean = np.mean(x1, axis=0)
         self.Sw = self.getSw(x0, x1)
-        print("Sw:",self.Sw)
         self.Sb = self.getSb()
         w = np.dot( self.inverse(self.Sw), self.x0_mean - self.x1_mean )
+        # w only decides the direction of the line,so we need to normalize it
         #self.weights = w/abs(w).min()
         self.weights = -w/w[-1]
 
@@ -52,29 +51,24 @@ class LDA:
         return self.weights
 
     def showDifference(self):
-        print("best:",abs(np.dot(self.weights,self.x0_mean)-np.dot(self.weights, self.x1_mean)))
-        random_one = np.array([1,2])
-        print("compare:",abs(np.dot(random_one,self.x0_mean)-np.dot(random_one, self.x1_mean)))
-
-    def projectPoint(self,x):
-        return np.dot(np.dot(self.weights,x), np.transpose(x))
-
+        weights = np.matrix(self.weights)
+        diff = self.x0_mean - self.x1_mean
+        print("best result:", weights * self.Sb * weights.T / (weights*self.Sw*weights.T))
+        temp = np.matrix([2,-1])
+        print("compared result:",temp* self.Sb * temp.T / (temp*self.Sw*temp.T))
 
 
-
-x0 = np.array([[1,2],[2,3]])
-x1 = np.array([[3,4],[4,4]])
+x0 = np.array([[0,1],[1,3],[2,2]])
+x1 = np.array([[6,2],[4,5],[5,8]])
 model = LDA()
 model.fit(x0,x1)
 model.showDifference()
+# show how the line and data look like (2dim data required)
 plt.scatter(x0[:,0],x0[:,1],marker='x')
 print("weights:", model.getWeights())
-plt.scatter(x1[:,0],x1[:,1],marker='x')
+plt.scatter(x1[:,0],x1[:,1],marker='+')
 x = np.linspace(0,4,1000)
 y = model.getWeights()[0]*x
 plt.plot(x,y)
 plt.show()
-
-
-
 
